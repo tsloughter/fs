370fb17e1cb6e7216d0356017cc81867371af5ad
Tristan Sloughter <t@crashfast.com>
2015-07-18 22:41:38 -0500

    refactor

--- src/fs.erl
+++ src/fs.erl
@@ -1,48 +1,27 @@
 -module(fs).
 -include_lib("kernel/include/file.hrl").
--export([subscribe/0, known_events/0, start_looper/0, path/0, find_executable/2]).
+-export([subscribe/0
+        ,subscribe/2
+        ,find_executable/1]).
 
-% sample subscriber
+-define(EVENTS, [created, closed, modified, moved, deleted, undefined]).
 
-subscribe() -> gen_event:add_sup_handler(fs_events, {fs_event_bridge, self()}, [self()]).
-known_events() -> gen_server:call(fs_server, known_events).
-start_looper() -> spawn(fun() -> subscribe(), loop() end).
+subscribe() ->
+    subscribe("", ?EVENTS).
 
-path() ->
-    case application:get_env(fs, path) of
-        {ok, P} -> filename:absname(P);
-        undefined -> filename:absname("") end.
+subscribe(Path, Events) ->
+    fs_server:start_link(filename:absname(Path), Events).
 
-loop() ->
-    receive
-        {_Pid, {fs, file_event}, {Path, Flags}} -> error_logger:info_msg("file_event: ~p ~p", [Path, Flags]);
-        _ -> ignore end,
-    loop().
-
-find_executable(Cmd, DepsPath) ->
-    case priv_file(Cmd) of
-    false -> mad_file(DepsPath);
-    Priv  -> Priv end.
-
-mad_file(DepsPath) ->
-    case filelib:is_regular(DepsPath) of
-    true  -> DepsPath;
-    false ->
-        case mad_repl:load_file(DepsPath) of
-        {error,_} ->
-            %% This path has been already checked in find_executable/2
-            false;
-        {ok,ETSFile} ->
-            filelib:ensure_dir(DepsPath),
-            file:write_file(DepsPath, ETSFile),
-            file:write_file_info(DepsPath, #file_info{mode=8#00555}) end end.
-
-priv_file(Cmd) ->
-    case code:priv_dir(fs) of
-    Priv when is_list(Priv) ->
-        Path = filename:join(Priv, Cmd),
-        case filelib:is_regular(Path) of
-        true  -> Path;
-        false -> false end;
-    _ ->
-        false end.
+find_executable(Cmd) ->
+    case code:priv_dir(?MODULE) of
+        {error, bad_name} ->
+            error_logger:info_msg("Executable ~s not found", [Cmd]);
+        Priv ->
+            Path = filename:join(Priv, Cmd),
+            case filelib:is_regular(Path) of
+                true  ->
+                    Path;
+                false ->
+                    error_logger:info_msg("Executable ~s not found", [Cmd])
+            end
+    end.
--- src/fs_server.erl
+++ src/fs_server.erl
@@ -1,26 +1,75 @@
 -module(fs_server).
+
 -behaviour(gen_server).
+
+-export([start_link/2]).
+
+-export([init/1
+        ,handle_call/3
+        ,handle_cast/2
+        ,handle_info/2
+        ,terminate/2
+        ,code_change/3]).
+
 -define(SERVER, ?MODULE).
--export([start_link/3]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,terminate/2, code_change/3]).
 
--record(state, {port, path, backend}).
+-type event() :: created | closed | modified | moved | deleted | undefined.
+-record(state, { port    :: port(),
+                 path    :: file:filename_all(),
+                 re      :: re:mp(),
+                 events  :: [event()],
+                 backend :: fsevents | inotifywait | inotifywait_win32,
+                 pid     :: pid() }).
 
-notify(file_event = A, Msg) -> Key = {fs, A}, gen_event:notify(fs_events, {self(), Key, Msg}).
-start_link(Backend, Path, Cwd) -> gen_server:start_link({local, ?SERVER}, ?MODULE, [Backend, Path, Cwd], []).
-init([Backend, Path, Cwd]) -> {ok, #state{port=Backend:start_port(Path, Cwd),path=Path,backend=Backend}}.
+start_link(Path, Events) ->
+    case os:type() of
+        {unix, darwin} ->
+            gen_server:start_link(?MODULE, [fsevents, Path, Events, self()], []);
+        {unix, linux} ->
+            gen_server:start_link(?MODULE, [inotifywait, Path, Events, self()], []);
+        {win32, nt} ->
+            gen_server:start_link(?MODULE, [inotifywait_win32, Path, Events, self()], []);
+        _ ->
+            {error, no_backend}
+    end.
 
-handle_call(known_events, _From, #state{backend=Backend} = State) -> {reply, Backend:known_events(), State};
-handle_call(_Request, _From, State) -> {reply, ok, State}.
-handle_cast(_Msg, State) -> {noreply, State}.
-handle_info({_Port, {data, {eol, Line}}}, #state{backend=Backend} = State) ->
-    Event = Backend:line_to_event(Line),
-    notify(file_event, Event),
-    {noreply, State};
+init([Backend, Path, Events, Pid]) ->
+    {ok, #state{port=Backend:start_port(Path)
+               ,re=Backend:line_parser()
+               ,path=Path
+               ,events=sets:from_list(Events)
+               ,backend=Backend
+               ,pid=Pid}}.
+
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info({_Port, {data, {eol, Line}}}, #state{backend=Backend
+                                                ,re=RE
+                                                ,events=ListenEvents
+                                                ,pid=Pid}=State) ->
+    {EventPath, Events} = Backend:line_to_event(Line, RE),
+    case sets:is_disjoint(ListenEvents, sets:from_list(Events)) of
+        true ->
+            {noreply, State};
+        false ->
+            Pid ! {EventPath, Events},
+            {noreply, State}
+    end;
 handle_info({_Port, {data, {noeol, Line}}}, State) ->
     error_logger:error_msg("~p line too long: ~p, ignoring~n", [?SERVER, Line]),
     {noreply, State};
-handle_info({_Port, {exit_status, Status}}, State) -> {stop, {port_exit, Status}, State};
-handle_info(_Info, State) -> {noreply, State}.
-terminate(_Reason, #state{port=Port}) -> (catch port_close(Port)), ok.
-code_change(_OldVsn, State, _Extra) -> {ok, State}.
+handle_info({_Port, {exit_status, Status}}, State) ->
+    {stop, {port_exit, Status}, State};
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, #state{port=Port}) ->
+    (catch port_close(Port)),
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
